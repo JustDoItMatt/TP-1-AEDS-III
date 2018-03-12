cbc79c86033752acfc7dd0677e1a1a468cbd4d77
JustDoItMatt <matheusasj@live.com>
2018-03-12 08:35:19 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,6 +1,10 @@
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
 
+struct kitBoom{
+	int type, nSerie;
+};
+typedef struct kitBoom bomb;
 char typeToChar(char );
 void criaMatriz();
 void attMatriz(int , int , char );
