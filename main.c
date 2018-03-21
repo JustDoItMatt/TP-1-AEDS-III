cc29999f292de0631806776898ada9e1c6b613e6
Pedrohap <37128553+Pedrohap@users.noreply.github.com>
2018-03-21 18:24:21 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 
 int main(int argc, char **argv)
 {
+	struct rusage r_inicio, r_fim;
 	struct timeval comeco, fim;
 	gettimeofday(&comeco, NULL);
 	//código a ser testado
 
 	readFile(argv[1]);
+	getrusage(RUSAGE_SELF,&r_usage);
 
 	gettimeofday(&fim, NULL);
+	getrusage(RUSAGE_SELF,&r_usage);
 	//tempo em ms para execução do cód
 	double t = (fim.tv_usec - comeco.tv_usec) / 100000.0;
 
 	printf("\n%.7f segundos\n", t);
+	printf("\nMemoria usada = %ld\n",r_usage.ru_maxrss)
 	//attMatriz();
 	return 0;
 }
